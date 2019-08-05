cf52fdc639cea505611a7ff81e93899d669ae969
herbncrypto <50765673+herbncrypto@users.noreply.github.com>
2019-08-04 19:27:50 -0700

    Update Developers.vue

--- src/views/Developers.vue
+++ src/views/Developers.vue
@@ -4,8 +4,8 @@
     <div class="p-4 after-header">
       <div class="container-sm mx-auto mb-4">
         <p class="mb-4">
-          You’ve found the SteemConnect developer documentation! This page dedicated to showing you
-          all the ways that you can use SteemConnect to make cool stuff.
+          You’ve found the SmokeConnect developer documentation! This page dedicated to showing you
+          all the ways that you can use SmokeConnect to make cool stuff.
         </p>
         <a
           @click="selectSection('new-app')"
@@ -16,10 +16,10 @@
         <div v-if="selectedSection === 'new-app'" class="mb-4">
           <div class="mb-3">
             <p>
-              To create a new app on SteemConnect you need a Steem account for it. If you don't have
+              To create a new app on SmokeConnect you need a Smoke account for it. If you don't have
               one yet you can create one on
-              <a href="https://signup.steemit.com" target="_blank">signup.steemit.com</a>. Once you
-              have a Steem account for your app, click on the button below, log in with your app
+              <a href="https://smoke.io" target="_blank">smoke.io</a>. Once you
+              have a Smoke account for your app, click on the button below, log in with your app
               account and update your profile with "application" as account type.
             </p>
             <router-link to="/profile" class="btn btn-large">
@@ -27,9 +27,9 @@
             </router-link>
           </div>
           <p>
-            If you would like to use the OAuth 2 API for posting with SteemConnect server you need
-            to authorize the Steem account "steemconnect" to post on the behalf of your app account.
-            <router-link to="/authorize/steemconnect">Click here</router-link>
+            If you would like to use the OAuth 2 API for posting with SmokeConnect server you need
+            to authorize the Smoke account "smokeconnect" to post on the behalf of your app account.
+            <router-link to="/authorize/smokeconnect">Click here</router-link>
             to do this and sign with your app account.
           </p>
         </div>
@@ -53,12 +53,12 @@
         </a>
         <div v-if="selectedSection === 'sdk'" class="mb-4">
           <p>
-            Get started integrating SteemConnect on your website with SteemConnect.js, the official
+            Get started integrating SmokeConnect on your website with SmokeConnect.js, the official
             JavaScript SDK.
           </p>
           <p>
-            <a href="https://github.com/steemscript/steemconnect.js" target="_blank"
-              ><span class="iconfont icon-mark-github" /> SteemConnect.js</a
+            <a href="https://github.com/herbncrypto/smokeconnect.js" target="_blank"
+              ><span class="iconfont icon-mark-github" /> SmokeConnect.js</a
             >
           </p>
         </div>
@@ -69,11 +69,11 @@
           <h4 class="m-0">Demo</h4>
         </a>
         <div v-if="selectedSection === 'demo'" class="mb-4">
-          <p class="mt-2">Checkout SteemConnect demo with Vue.js</p>
-          <p><a href="https://demo.steemconnect.com" target="_blank" class="mr-2">Try demo</a></p>
+          <p class="mt-2">Checkout SmokeConnect demo with Vue.js</p>
+          <p><a href="https://demo.smokeconnect.com" target="_blank" class="mr-2">Try demo</a></p>
           <p>
             <a
-              href="https://github.com/steemscript/steemconnect.js/blob/master/docs/index.html"
+              href="https://github.com/herbncrypto/smokeconnect.js/blob/master/docs/index.html"
               target="_blank"
             >
               <span class="iconfont icon-mark-github" /> See on GitHub
@@ -93,15 +93,15 @@
           </p>
           <p>
             <b>Base authorization URL</b><br />
-            https://api.steemconnect.com/api/oauth2/authorize
+            https://api.smokeconnect.com/api/oauth2/authorize
           </p>
           <p>
             <b>Token URL</b><br />
-            https://api.steemconnect.com/api/oauth2/token
+            https://api.smokeconnect.com/api/oauth2/token
           </p>
           <p>
             <b>Revocation URL</b><br />
-            https://api.steemconnect.com/api/oauth2/token/revoke
+            https://api.smokeconnect.com/api/oauth2/token/revoke
           </p>
         </div>
         <a
