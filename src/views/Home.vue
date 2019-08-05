8361c6cef35f90b58d7ce72e56180a38553f08d5
herbncrypto <50765673+herbncrypto@users.noreply.github.com>
2019-08-04 19:34:23 -0700

    Update Home.vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,7 +2,7 @@
   <div class="hero height-full">
     <Center>
       <span class="logo iconfont icon-steemconnect" />
-      <h4 class="m-0">steemconnect</h4>
+      <h4 class="m-0">smokeconnect</h4>
       <div class="mt-4 mb-7">
         <h1 class="mb-6">You're in control of your keys.</h1>
         <div class="columns mb-6 hide-sm">
@@ -28,7 +28,7 @@
           </div>
         </div>
         <p>
-          With SteemConnect, you're always in control of your private keys: we neither store nor
+          With SmokeConnect, you're always in control of your private keys: we neither store nor
           have access to them.
         </p>
       </div>
@@ -41,7 +41,7 @@
 import pkg from '@/../package.json';
 
 const chromeExtensionUrl =
-  'https://chrome.google.com/webstore/detail/steemconnect/enaifbbcamagcjaaeeaomhofifdgpjcn';
+  'https://chrome.google.com/webstore/detail/smokeconnect/enaifbbcamagcjaaeeaomhofifdgpjcn';
 
 export default {
   data() {
